2a41c2a55226c320d022815fb41346b10cff8528
MatejKrivec <matej.krivec6@gmail.com>
2024-06-22 21:44:16 +0200

    deploy adaptableio 2

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'https://geotagger-frontend-r1h7ucbx9-matej-krivecs-projects.vercel.app'
+    origin: 'https://geotagger-frontend-2qhi92mqj-matej-krivecs-projects.vercel.app'
   })
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
